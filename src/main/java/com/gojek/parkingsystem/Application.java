82f75b5c1c69231ea7758e7c5f5edee1ccc7a7b4
Arpit Pandey <arpit.pandey@tavant.com>
2018-03-05 13:00:02 +0530

    Added logger and function for processing requests

--- src/main/java/com/gojek/parkingsystem/Application.java
+++ src/main/java/com/gojek/parkingsystem/Application.java
@@ -1,41 +1,79 @@
 package com.gojek.parkingsystem;
 
+import com.gojek.parkingsystem.model.Vehicle;
 import com.gojek.parkingsystem.service.ParkingService;
+import com.gojek.parkingsystem.validation.ParkingValidator;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 public class Application {
+    static Logger log = LogManager.getLogger(Application.class);
 
+    ParkingService service;
+
+    public void performOperation(String[] params, int paramsLen) {
+        String command = params[0];
+        String output = "";
+        switch (command) {
+            case "create_parking_lot":
+                if (paramsLen == 2) {
+                    service = new ParkingService();
+                    output = service.createParking(Integer.parseInt(params[1]));
+                } else {
+                    log.error("Insufficient parameters passed" + params.toString());
+                }
+                break;
+            case "park":
+                if (paramsLen == 3 && ParkingValidator.validateRegistrationNumber(params[1])) {
+                    output = service.parkVehicle(new Vehicle(params[1], params[2]));
+                } else {
+                    log.error("Insufficient parameters passed" + params.toString());
+                }
+                break;
+            case "leave":
+                if(paramsLen == 2) {
+                    output = service.removeVehicle(Integer.parseInt(params[1]));
+                } else {
+                    log.error("Insufficient parameters passed {}" + params.toString());
+                }
+                break;
+            case "status":
+
+                break;
+            case "registration_numbers_for_cars_with_colour":
+                if(paramsLen == 2) {
+                    output = service.getRegistrationNumberForColor(params[1]);
+                } else {
+                    log.error("Insufficient parameters passed {}" + params.toString());
+                }
+                break;
+            case "slot_numbers_for_cars_with_colour":
+                if(paramsLen == 2) {
+                    output = service.getSlotNumberForColor(params[1]);
+                } else {
+                    log.error("Insufficient parameters passed {}" + params.toString());
+                }
+                break;
+            case "slot_number_for_registration_number":
+                if(paramsLen == 2) {
+                    output = service.getSlotNumberForRegistration(params[1]);
+                } else {
+                    log.error("Insufficient parameters passed {}" + params.toString());
+                }
+                break;
+            case "exit":
+                System.exit(0);
+        }
+    }
 
     public static void main(String[] args) {
         int paramsCount = args.length;
-        ParkingService service;
+
         boolean isParkingLotCreated = false;
 
         if(paramsCount > 0) {
             String command = args[0];
-            switch (command) {
-                case "create_parking_lot":
-                    service = new ParkingService();
-                    break;
-                case "park":
 
-                    break;
-                case "leave":
-
-                    break;
-                case "status":
-
-                    break;
-                case "registration_numbers_for_cars_with_colour":
-
-                    break;
-                case "slot_numbers_for_cars_with_colour":
-
-                    break;
-                case "slot_number_for_registration_number":
-
-                    break;
-                case "exit":
-                    System.exit(0);
             }
         }
     }
--- src/main/java/com/gojek/parkingsystem/validation/ParkingValidator.java
+++ src/main/java/com/gojek/parkingsystem/validation/ParkingValidator.java
@@ -1,7 +1,18 @@
 package com.gojek.parkingsystem.validation;
 
-public class ParkingValidator {
-    public void validateRegistrationNumber(String registrationNumber) {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class ParkingValidator {
+
+    public static boolean isMatch(String regex, String text) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        return matcher.find();
+    }
+
+    public static boolean validateRegistrationNumber(String registrationNumber) {
+        String registrationNumberRegex = "[A-Z]{2}-[0-9]{2}-[A-Z]{2}-[0-9]{4}";
+        return isMatch(registrationNumberRegex, registrationNumber);
     }
 }
